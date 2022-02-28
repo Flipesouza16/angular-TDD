3851bce085aee49449a25c8788e170534b1c01cc
FilipeSouza <filipe.souza1602@gmail.com>
2022-02-27 23:06:14 -0300

    feat: create verifyAllField function to validate all fields

--- src/app/components/login/services/authentication.service.ts
+++ src/app/components/login/services/authentication.service.ts
@@ -1,20 +1,63 @@
 import { Injectable } from '@angular/core';
-import { Authentication } from '../interfaces/authentication';
+import { AuthenticationParams } from 'src/domain/useCases/authentication';
+import { CheckedFieldsType } from '../interfaces/authentication-validation';
+import { HttpClientService } from 'src/infra/http-client-service';
+import { Observable } from 'rxjs';
 
+enum AuthenticationFields {
+  firstName = 'firstname',
+  email = 'email',
+  password = 'password',
+  id = 'id'
+}
 @Injectable({
   providedIn: 'root',
 })
+
 export class AuthenticationService {
-  authentication: Authentication = {
+  authentication: AuthenticationParams = {
     email: '',
     password: '',
   };
+  endPoint = 'http://localhost:3000/users'
 
-  constructor() {}
+  checkedFields = false;
+
+  constructor(private httpClientService: HttpClientService) {}
 
   login() {}
 
-  register() {}
+  register(params: AuthenticationParams): Observable<any> {
+    const { fieldInvalid, isAllFieldsFilledAndValidated } = this.verifyAllField(params);
+    if(isAllFieldsFilledAndValidated) {
+      return this.httpClientService.post({ url: this.endPoint, body: params });
+    } else {
+      throw Error(fieldInvalid)
+    }
+  }
+
+  verifyAllField(params: AuthenticationParams): CheckedFieldsType {
+    let fieldInvalid: any;
+    let isAllFieldsFilledAndValidated = true;
+
+    for(const typeField of Object.keys(params)) {
+      if(typeField === AuthenticationFields.firstName && !params.firstname?.length) {
+        fieldInvalid = AuthenticationFields.firstName;
+      } else if(typeField === AuthenticationFields.email && !this.validateEmail(params.email)) {
+        fieldInvalid = AuthenticationFields.email;
+      } else if(typeField === AuthenticationFields.password && !this.validatePassowrdStrength(params.password)) {
+        fieldInvalid = AuthenticationFields.password;
+      }
+      if(fieldInvalid) isAllFieldsFilledAndValidated = false;
+    }
+
+    this.checkedFields = true;
+
+    return {
+      isAllFieldsFilledAndValidated,
+      fieldInvalid
+    }
+  }
 
   validateEmail(email: string): boolean {
     var regex = /\S+@\S+\.\S+/;
@@ -22,9 +65,7 @@ export class AuthenticationService {
   }
 
   validatePassowrdStrength(password: string): boolean {
-    var mediumRegex = new RegExp(
-      '^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})'
-    );
+    var mediumRegex = /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})/;
     return mediumRegex.test(password);
   }
 }
