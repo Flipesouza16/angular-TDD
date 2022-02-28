f6c1c1c164f7eb3173e50b3c395eb8de1543f098
FilipeSouza <filipe.souza1602@gmail.com>
2022-02-28 18:22:58 -0300

    refactor: adjust http as per HttpPostClient interface

--- src/infra/http-client-service.ts
+++ src/infra/http-client-service.ts
@@ -1,17 +1,24 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { HttpPostParams } from "src/data/protocols/http/http-post-client";
+import { HttpPostClient, HttpPostParams } from "src/data/protocols/http/http-post-client";
+import { HttpResponse } from "src/data/protocols/http/http-response";
 
 @Injectable({
   providedIn: 'root',
 })
 
-export class HttpClientService {
+export class HttpClientService implements HttpPostClient<any, any> {
 
   constructor(private http: HttpClient) {}
 
-  post(params: HttpPostParams<any>): Observable<any> {
-    return this.http.post(params.url, params.body);
+  post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    return new Promise(resolve => {
+      this.http.post(params.url, params.body).subscribe((data: any) => {
+        resolve({
+          statusCode: data.status,
+          body: data
+        });
+      })
+    })
   }
 }
