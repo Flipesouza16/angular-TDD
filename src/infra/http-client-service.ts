1bf1e2a4451a825ddd3b0357c80186f76f050c1f
FilipeSouza <filipe.souza1602@gmail.com>
2022-03-29 20:58:52 -0300

    feat: add AccountModel to the httpResponse in http-client-service

--- src/infra/http-client-service.ts
+++ src/infra/http-client-service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { HttpPostClient, HttpPostParams } from "src/data/protocols/http/http-post-client";
 import { HttpResponse } from "src/data/protocols/http/http-response";
-
+import { AccountModel } from "src/domain/models/account-model";
 @Injectable({
   providedIn: 'root',
 })
@@ -11,11 +11,10 @@ export class HttpClientService implements HttpPostClient<any, any> {
 
   constructor(private http: HttpClient) {}
 
-  post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+  post(params: HttpPostParams<any>): Promise<HttpResponse<AccountModel>> {
     return new Promise(resolve => {
       this.http.post(params.url, params.body).subscribe((data: any) => {
         resolve({
-          statusCode: data.status,
           body: data
         });
       })
