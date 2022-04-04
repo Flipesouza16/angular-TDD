3517a11c9b3433c9d9ea6bf961f0588eb6462600
FilipeSouza <filipe.souza1602@gmail.com>
2022-04-03 21:54:18 -0300

    feat: create login and register template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientService } from 'src/infra/http-client-service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [HttpClientService, HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,34 @@
-import { Component } from "@angular/core";
-
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['login.component.scss']
 })
 
-export class LoginComponent {}
+export class LoginComponent implements OnInit {
+  public isLogin = true;
+  public formLogin: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+   this.formLogin = this.formBuilder.group({
+     name: [null, [Validators.required]],
+     email: [null, [Validators.required, Validators.email]],
+     password: [null, [Validators.required, Validators.minLength(6)]],
+   });
+   console.log('this.formLogin: ',this.formLogin);
+  }
+
+  onSubmit() {
+    if(this.isLogin) {
+    } else {
+    }
+  }
+
+  toggleLoginRegister() {
+    this.isLogin = !this.isLogin;
+  }
+
+}
