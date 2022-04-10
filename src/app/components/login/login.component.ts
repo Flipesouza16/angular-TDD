a35b7de3eb23fccaaa119b40b77365c8c00ac48c
FilipeSouza <filipe.souza1602@gmail.com>
2022-04-10 20:14:17 -0300

    feat: adjust code of login/register and finish register function

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -20,14 +20,20 @@ export class LoginComponent implements OnInit {
     atLeast1Special: false,
     mustBe8CharactersOrLonger: false,
   };
+  public isFormInvalid = false;
+  public isLoading = false;
 
   constructor(private formBuilder: FormBuilder, private authenticationService: AuthenticationService, private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
-   this.formLogin = this.formBuilder.group({
-     email: ['', [Validators.required, Validators.email]],
-     password: ['', [Validators.required, Validators.minLength(8)]],
-   });
+    this.initializeFormLogin();
+  }
+
+  initializeFormLogin(): void {
+    this.formLogin = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
   }
 
   toggleLoginRegister(): void {
@@ -39,26 +45,8 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  async register() {
-    if(!this.formLogin.valid) {
-      this.validateForm();
-    } else {
-      const authenticationParams: AuthenticationParams = {
-        firstname: this.formLogin.get('name')?.value,
-        email: this.formLogin.get('email')?.value,
-        password: this.formLogin.get('password')?.value
-      }
-      try {
-        // const response = await this.authenticationService.register(authenticationParams);
-        // console.log('response: ',response);
-      } catch(error) {
-        console.error(error);
-      }
-    }
-  }
-
   validateForm(): void {
-    let invalidField = null;
+    let invalidField = '';
     const formControls = this.formLogin.controls;
     for(const nameField in formControls) {
       if(formControls['name'].invalid) {
@@ -70,12 +58,15 @@ export class LoginComponent implements OnInit {
       }
     }
 
-    this.validateEachStageOfPassword();
+    if(invalidField) {
+      this.isFormInvalid = true;
+    }
 
+    this.validateEachStageOfPassword(invalidField);
     invalidField && this.openSnackBar(`The ${invalidField} field is invalid`);
   }
 
-  validateEachStageOfPassword(): void {
+  validateEachStageOfPassword(invalidField: string): void {
     const formControls = this.formLogin.controls;
     const password = formControls['password'].value;
     const typesOfValidationOfPassword = Object.keys(this.authenticationService.fullValidationRequirements(password));
@@ -86,8 +77,14 @@ export class LoginComponent implements OnInit {
       if(isPasswordStageValid) {
         this.fullValidationRequirements[stageValidationPassword] = true;
       } else {
+        this.fullValidationRequirements[stageValidationPassword] = false;
         this.isPasswordInvalid = true;
       }
+    }
+
+    if(invalidField !== 'password' && this.isPasswordInvalid ) {
+      this.openSnackBar(`The password field is invalid`);
+      this.isFormInvalid = true;
     }
   }
 
@@ -95,6 +92,30 @@ export class LoginComponent implements OnInit {
     this.snackBar.open(message, 'Close', {
       duration
     });
+  }
+
+  async register() {
+    this.validateForm();
+    if(!this.isFormInvalid) {
+      const authenticationParams: AuthenticationParams = {
+        firstname: this.formLogin.get('name')?.value,
+        email: this.formLogin.get('email')?.value,
+        password: this.formLogin.get('password')?.value
+      }
+      try {
+        this.isLoading = true;
+        const response = await this.authenticationService.register(authenticationParams);
+        console.log('response: ',response);
+        this.openSnackBar('Registered successfully!');
+        setTimeout(() => {
+          window.location.reload();
+          this.isLoading = false;
+        }, 1000)
+      } catch(error) {
+        console.error(error);
+        this.isLoading = false;
+      }
+    }
   }
 
   async onSubmit(): Promise<void> {
