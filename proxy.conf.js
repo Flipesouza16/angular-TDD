ffac31a72c4e5582414dac16269e463d91559530
FilipeSouza <filipe.souza1602@gmail.com>
2022-04-16 17:17:31 -0300

    fix: update target of proxy_config

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
   {
     context: ['/api'],
-    target: 'https://localhost:3000/',
+    target: 'http://localhost:3000/',
     secure: false,
     logLevel: 'debug',
     pathRewrite: {'^/api': '' }
